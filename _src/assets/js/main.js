5dec063ff817f30ec7f5c89005429badb86ada7f
Migara <evamarcod@gmail.com>
2019-09-23 17:16:00 +0200

    Add tabs for skills

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -8,6 +8,7 @@ const modal = document.querySelector('.modal');
 const closeBtn = document.querySelector('.close');
 const modalImage = document.querySelectorAll('.image');
 const closeProyect = document.querySelectorAll('.close-project');
+const skillLinks = document.querySelectorAll('.skill__link');
 
 function openMenu (){
   modal.classList.add('one');
@@ -35,8 +36,23 @@ function closeInfo(event){
     modalProject.classList.toggle('out');
   }
 }
-
-
+function changeTab(event){
+  const target = event.target.id;
+  const div1 = document.getElementsByClassName('1');
+  const div2 = document.getElementsByClassName('2');
+  const div3 = document.getElementsByClassName('3');
+  const div4 = document.getElementsByClassName('4');
+  const div5 = document.getElementsByClassName('5');
+  div1[0].classList.add('hidden');
+  div2[0].classList.add('hidden');
+  div3[0].classList.add('hidden');
+  div4[0].classList.add('hidden');
+  div5[0].classList.add('hidden');
+  console.log(target);
+  const divTarget = document.getElementsByClassName(target);
+  console.log(divTarget);
+  divTarget[0].classList.toggle('hidden');
+}
 
 for(const item of modalImage){
   item.addEventListener('click', openInfo);
@@ -47,4 +63,8 @@ for(const item of closeProyect){
 if(menuBtn){
   menuBtn.addEventListener('click', openMenu);
   closeBtn.addEventListener('click', closeMenu);
+}
+
+for(const item of skillLinks){
+  item.addEventListener('click', changeTab);
 }
